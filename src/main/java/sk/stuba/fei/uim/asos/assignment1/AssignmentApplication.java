0b2506eaa01d2bae30645fc4488e623d6be92485
Milan Mladoniczky <6153201+mladoniczky@users.noreply.github.com>
2020-10-13 23:28:53 +0200

    First commit

---
+++ src/main/java/sk/stuba/fei/uim/asos/assignment1/AssignmentApplication.java
@@ -0,0 +1,15 @@
+package sk.stuba.fei.uim.asos.assignment1;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.logging.Logger;
+
+public class AssignmentApplication {
+
+    private static final Logger log = Logger.getLogger(AssignmentApplication.class.getSimpleName());
+
+    public static void main(String[] args) {
+
+    }
+}
